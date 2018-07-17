75b264ea482d986e78a63927a831f501bd1b820f
kaushnian <kefbox@gmail.com>
2018-07-17 16:16:00 -0700

    Added declaring a winner

--- react/tic-tac-toe/src/index.js
+++ react/tic-tac-toe/src/index.js
@@ -22,7 +22,13 @@ class Board extends React.Component {
   handleClick(i) {
     // Makes a copy.
     const squares = this.state.squares.slice();
-    squares[i] = this.getNext();
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.getCurrentPlayer();
+    
     this.setState({
       squares: squares,
       xIsNext: !this.state.xIsNext,
@@ -38,12 +44,20 @@ class Board extends React.Component {
     );
   }
 
-  getNext() {
+  getCurrentPlayer() {
     return this.state.xIsNext ? 'X' : 'O';
   }
 
   render() {
-    const status = `Next player: ${this.getNext()}`;
+    const winner = calculateWinner(this.state.squares);
+
+    let status;
+
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${this.getCurrentPlayer()}`;
+    }
 
     return (
       <div>
@@ -90,4 +104,27 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
 
